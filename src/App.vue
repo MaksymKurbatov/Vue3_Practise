13b574321de28414ac7e412a1bab65e11f084ca3
Maksym_Kurbatov <Maksym_Kurbatov@epam.com>
2023-05-29 18:23:55 +0300

    createPost

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,92 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Create post</h4>
+      <input
+          v-bind:value="title"
+          @input="title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Name">
+      <input
+          v-bind:value="body"
+          class="input"
+          @input="body = $event.target.value"
+          type="text"
+          placeholder="Desc">
+      <button class="btn" @click="createPost">Create</button>
+    </form>
+    <div class="post" v-for="post in posts" :key="post.id">
+      <div><strong>Name</strong> {{ post.title }}</div>
+      <div><strong>Desc</strong> {{ post.body }}</div>
+    </div>
+
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+      body: "",
+      posts: [
+        {id: 1, title: 'Javascript', body: 'someDescription 1'},
+        {id: 2, title: 'TypeScrip', body: 'someDescription 2 '},
+        {id: 3, title: 'PHP', body: 'someDescription 3'},
+      ]
+    }
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost);
+      this.title = "";
+      this.body = "";
+    },
+  }
+}
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.post {
+  padding: 15px;
+  border: 2px solid darkslategray;
+  margin-top: 15px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  margin-top: 10px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: darkslategray;
+  border: 1px solid darkslategray;
+
+}
+
+.input {
+  width: 100%;
+  border: 1px solid darkslategray;
+  padding: 10px 15px;
+  margin-top: 15px;
 }
 </style>
