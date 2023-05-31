381aebcfd9846ed27942ce11eac047b8acaf5267
Maksym_Kurbatov <Maksym_Kurbatov@epam.com>
2023-05-31 15:25:00 +0300

    Revert "Create post decomposition"
    This reverts commit 21fe70e7122e84bdf8c067d78eed93f6c25d47f2.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="app">
-   <post-form
-       @create = "createPost"
-   />
-   <post-list
-       :posts="posts"
-   />
+    <form @submit.prevent>
+      <h4>Create post</h4>
+      <input
+          v-bind:value="title"
+          @input="title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Name">
+      <input
+          v-bind:value="body"
+          class="input"
+          @input="body = $event.target.value"
+          type="text"
+          placeholder="Desc">
+      <button class="btn" @click="createPost">Create</button>
+    </form>
+    <div class="post" v-for="post in posts" :key="post.id">
+      <div><strong>Name</strong> {{ post.title }}</div>
+      <div><strong>Desc</strong> {{ post.body }}</div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import PostForm from "@/components/PostForm/PostForm";
-import PostList from "@/components/PostList/PostList";
-
 export default {
-  components: {
-    PostForm,
-    PostList
-  },
   data() {
     return {
+      title: "",
+      body: "",
       posts: [
         {id: 1, title: 'Javascript', body: 'someDescription 1'},
         {id: 2, title: 'TypeScrip', body: 'someDescription 2 '},
@@ -28,9 +38,16 @@ export default {
     }
   },
   methods: {
-    createPost(post){
-    this.posts.push(post);
-    }
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost);
+      this.title = "";
+      this.body = "";
+    },
   }
 }
 </script>
@@ -45,5 +62,31 @@ export default {
   padding: 20px;
 }
 
+.post {
+  padding: 15px;
+  border: 2px solid darkslategray;
+  margin-top: 15px;
+}
 
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.btn {
+  margin-top: 10px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: darkslategray;
+  border: 1px solid darkslategray;
+
+}
+
+.input {
+  width: 100%;
+  border: 1px solid darkslategray;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
 </style>
